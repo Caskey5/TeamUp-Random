6ba4ee7423a35474066fcd9ae1aea7958fd31fd2
Antonio Šiljac <siljacantonio@gmail.com>
2025-12-13 21:35:24 +0100

    Site is working!

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,15 @@ from .views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    
+    path('about/', HomePageView.as_view(), name='about'),
+    path('clients/', HomePageView.as_view(), name='clients'),
+    path('contact/', HomePageView.as_view(), name='contact'),
+    path('footer/', HomePageView.as_view(), name='footer'),
+    path('masthead/', HomePageView.as_view(), name='masthead'),
+    path('navigation/', HomePageView.as_view(), name='navigation'),
+    path('portfolio_grid/', HomePageView.as_view(), name='portfolio_grid'),
+    path('portfolio_models/', HomePageView.as_view(), name='portfolio_models'),
+    path('services/', HomePageView.as_view(), name='services'),
+    path('team/', HomePageView.as_view(), name='team'),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,40 @@
 from django.views.generic import TemplateView
 
-# Index page
+# Home page
 class HomePageView(TemplateView):
     template_name = "base/home.html"
+
+
+
+# Additional page views
+class AboutView(TemplateView):
+    template_name = "pages/about.html"
+
+class ClientsView(TemplateView):
+    template_name = "pages/clients.html"
+
+class ContactView(TemplateView):
+    template_name = "pages/contact.html"
+
+class FooterView(TemplateView):
+    template_name = "pages/footer.html"
+
+class MastheadView(TemplateView):
+    template_name = "pages/masthead.html"
+
+class NavigationView(TemplateView):
+    template_name = "pages/navigation.html"
+
+class PortfolioGridView(TemplateView):
+    template_name = "pages/portfolio_grid.html"
+
+class PortfolioModelsView(TemplateView):
+    template_name = "pages/portfolio_models.html"
+
+class ServicesView(TemplateView):
+    template_name = "pages/services.html"
+
+class TeamView(TemplateView):
+    template_name = "pages/team.html"
+
+
