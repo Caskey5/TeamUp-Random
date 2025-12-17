ff4c400ee275ef83b20d3e9de249985c16bc2ad1
Antonio Šiljac <siljacantonio@gmail.com>
2025-12-17 20:51:23 +0100

    Update urls.py

--- generate_team/urls.py
+++ generate_team/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ChooseSportView
 
 urlpatterns = [
-    path('choose_sport/', ChooseSportView.as_view(), name='choose_sport'),
+    path('', ChooseSportView.as_view(), name='choose_sport'),
     #path('generate_team/', GeneratedTeamView.as_view(), name='generate_team'),
 ]
